f66f1eca4a05b08db04b57bffacd8918dafab1d2
joneqian <qian.qing@aliyun.com>
2016-01-20 16:03:54 +0800

    product list

---
+++ routes/product.js
@@ -0,0 +1,18 @@
+/**
+ * @author qianqing
+ * @create by 16-1-20
+ * @description
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/new', function(req, res, next) {
+	res.render('product-new', { title: '新品' });
+});
+
+router.get('/sale', function(req, res, next) {
+	res.render('product-sale', { title: '特卖' });
+});
+
+module.exports = router;
