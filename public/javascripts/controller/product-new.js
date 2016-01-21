f3f7341b49e827db5085bd9aac833a28e40c68df
joneqian <qian.qing@aliyun.com>
2016-01-21 11:50:49 +0800

    product list

--- public/javascripts/controller/product-new.js
+++ public/javascripts/controller/product-new.js
@@ -47,7 +47,11 @@ require(['Vue'],
 				// 生成新条目的HTML
 				var html = '';
 				for (var i = lastIndex + 1; i <= lastIndex + number; i++) {
-					html += '<li class="item-content"><div class="item-inner"><div class="item-title">新条目</div></div></li>';
+					html += '<li class="item-content"><div class="item-media"><a href="#"><img src="' + vm.products[i%3].img+
+							'"></a></div><div class="item-inner"><div class="item-title-row"><div class="item-title">' +
+							vm.products[i%3].name + '</div></div><div class="item-subtitle"><span class="price-text">' +
+							vm.products[i%3].price + '</span><p class="buttons-row"><a href="#" class="button button-fill button-warning">' +
+							'收藏</a>' +	'<a href="#" class="button button-fill button-success">购买</a></p></div></div></div></li>';
 				}
 				// 添加新条目
 				$('.infinite-scroll .list-container').append(html);
