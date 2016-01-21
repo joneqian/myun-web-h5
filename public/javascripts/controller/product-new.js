00688c5bb189d86cdce3329b962f2bb812ca87f4
joneqian <qian.qing@aliyun.com>
2016-01-21 09:50:56 +0800

    product list

--- public/javascripts/controller/product-new.js
+++ public/javascripts/controller/product-new.js
@@ -18,10 +18,6 @@ require.config({
 require(['Vue'],
 	function (Vue) {
 		'use strict';
-		$(document).on('pageInit', '#page-product', function (e, id, page) {
-
-		})
-		$.init();
 		var vm = new Vue({
 			el: '#page-product',
 			data: {
@@ -38,5 +34,49 @@ require(['Vue'],
 				]
 			}
 		});
+
+		$(document).on("pageInit", "#page-product", function(e, id, page) {
+			var loading = false;
+			// 每次加载添加多少条目
+			var itemsPerLoad = 10;
+			// 最多可加载的条目
+			var maxItems = 100;
+			var lastIndex = $('.list-container li').length;
+
+			function addItems(number, lastIndex) {
+				// 生成新条目的HTML
+				var html = '';
+				for (var i = lastIndex + 1; i <= lastIndex + number; i++) {
+					html += '<li class="item-content"><div class="item-inner"><div class="item-title">新条目</div></div></li>';
+				}
+				// 添加新条目
+				$('.infinite-scroll .list-container').append(html);
+			}
+
+			$(page).on('infinite', function() {
+				console.log('loading:' + loading);
+				// 如果正在加载，则退出
+				if (loading) return;
+				// 设置flag
+				loading = true;
+				// 模拟1s的加载过程
+				setTimeout(function() {
+					// 重置加载flag
+					loading = false;
+					if (lastIndex >= maxItems) {
+						// 加载完毕，则注销无限加载事件，以防不必要的加载
+						$.detachInfiniteScroll($('.infinite-scroll'));
+						// 删除加载提示符
+						$('.infinite-scroll-preloader').remove();
+						return;
+					}
+					addItems(itemsPerLoad,lastIndex);
+					// 更新最后加载的序号
+					lastIndex = $('.list-container li').length;
+					$.refreshScroller();
+				}, 300);
+			});
+		});
+		$.init();
 	}
 );
